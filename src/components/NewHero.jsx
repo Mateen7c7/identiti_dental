e36d61f4bbfe0d4fc5e8366554bf3248e2355f56
Mateen Azfar <mateenazfar786@gmail.com>
2025-12-24 09:24:13 +0530

    Refactored NewHero component: updated layout with additional video sections, improved spacing, and adjusted text formatting for better readability.

--- src/components/NewHero.jsx
+++ src/components/NewHero.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import VideoCarousel from './VideoCarousel';
+import React from "react";
+import VideoCarousel from "./VideoCarousel";
 
 const NewHero = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#FFFFF0] px-5  overflow-hidden font-sans text-center">
-      <div className="flex flex-col w-full  max-w-md z-10 gap-6">
-        
+      <div className="flex flex-col w-full  max-w-md z-10 gap-6 mt-30">
         {/* Main Title */}
         <h1 className="text-5xl font-bold animate-fade-in-up">
           <span className="block text-[#F05B28]">IDENTITI</span>
@@ -24,11 +23,11 @@ const NewHero = () => {
           </p>
           <span className="h-px w-12 bg-linear-to-l from-transparent to-[#F05B28]"></span>
         </div>
-        
+
         {/* Subtitle */}
         <p className="text-md text-gray-600 leading-relaxed px-2 animate-fade-in-up-delay-2">
-          Experience exceptional dental care in a modern, comfortable environment. 
-          We're committed to making your smile our top priority.
+          Experience exceptional dental care in a modern, comfortable
+          environment. We're committed to making your smile our top priority.
         </p>
 
         {/* CTA Button */}
@@ -36,9 +35,38 @@ const NewHero = () => {
           Book Appointment
         </button>
 
-        {/* Placeholder Section */}
-        <div className="">
-          <VideoCarousel />
+        <div className="flex flex-col gap-6">
+          {/* Video Section */}
+        <div className="rounded-2xl overflow-hidden">
+          <video
+            src="/vid3.mp4"
+            className="w-full h-[220px] sm:h-[350px] md:h-[420px] object-cover"
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+        </div>
+        <div className="rounded-2xl overflow-hidden">
+          <video
+            src="/all2.mp4"
+            className="w-full h-[220px] sm:h-[350px] md:h-[420px] object-cover"
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+        </div>
+        <div className="rounded-2xl overflow-hidden">
+          <video
+            src="/vid2.MP4"
+            className="w-full h-[220px] sm:h-[350px] md:h-[420px] object-cover"
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+        </div>
         </div>
       </div>
     </div>
